006e06c89663285967501fac58521bf2fc441284
abdul karim <khamardanga005@gmail.com>
2024-07-08 23:54:47 +0600

    print function,F stringVariables Type Checking

--- test.py
+++ test.py
@@ -1,29 +1,82 @@
-#data type
+# # #data type
 
-a=5   #intger
-print("type of a =",type(a))
+# # a=5   #intger
+# # print("type of a =",type(a))
 
-b=4.3   #float
-print("type of b =",type(b))
+# # b=4.3   #float
+# # print("type of b =",type(b))
  
  
-c="sohan"   #string
-print("type of c =",type(c))
+# # # c="sohan"   #string
+# # # print("type of c =",type(c))
 
-d=(3,4,6,"sohan","school")     # tuple
-d=(3,4,6,"sohan","school")     # tuple
-d=(3,4,6,"sohan","school")     # tuple
-print("type of d=",type(d))
+# # # d=(3,4,6,"sohan","school")     # tuple
+# # # d=(3,4,6,"sohan","school")     # tuple
+# # # d=(3,4,6,"sohan","school")     # tuple
+# # # print("type of d=",type(d))
 
-e={'3','5','mubin','a','d'}    #set
-print("type of e=",type(e))
+# # # e={'3','5','mubin','a','d'}    #set
+# # # print("type of e=",type(e))
 
-f=['h','h','j','r','k','kawser']#liste   
-print("type of f =",type(f))
+# # # f=['h','h','j','r','k','kawser']#liste   
+# # # print("type of f =",type(f))
 
-g=True #or False  #boolean
-print("type of g =",type(g))
+# # # g=True #or False  #boolean
+# # # print("type of g =",type(g))
 
-h={"Name":"rayhan","Department":"CSE","ID":8895018} #dictionary
-print("type of h =",type(h))
+# # # h={"Name":"rayhan","Department":"CSE","ID":8895018} #dictionary
+# # # print("type of h =",type(h))
 
+
+# # # Print Function
+# # name = "Rakib"
+# # b = 10
+# # c = 11.50
+
+# # print(name, b, c)
+
+# # ##print function 
+# # name ="karim"
+# # age=20
+# # address ="rajbari"
+# # # print("Name: ",name,"," "age: ",age, ", " "address: ",address)
+
+# # ##F string
+# # # print(f "Name: ",{name}, age: {age}, address: {address}")
+      
+# # print("name: {}, age: {}, addreas: {}".format(name,age,address))      
+
+
+
+# # Variables Type Checking
+# a = 10
+# a:int = 10
+
+
+# b = "Rakib"
+# b:str = "Rakib"
+
+# c = 12.25
+# c:float = 12.25
+
+# d = a
+# e = 't'
+
+# print(type(a))
+# print(type(b))
+# print(type(c))
+# print(d)
+##variable types checking
+# a=23
+# a:int =23
+
+# b=41.3
+# b: float =41.3
+
+# c="a.karim"
+# c:string = "a.karim"
+
+
+# print(type(a))
+# print(type(b))
+# print(type(c))
